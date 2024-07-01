043d8ae5638f1f25551a0d9ec9d1bd88b42ff78a
Sandro Dias <133690725+gitdias@users.noreply.github.com>
2024-06-30 22:19:44 -0300

    Create configureNix.sh
    Only header

---
+++ configureNix.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#==============================================================================
+#
+#          FILE:  configureNix.sh
+# 
+#         USAGE:  ./configureNix.sh [options]
+# 
+#   DESCRIPTION:    PT = "Este script é parte de projeto pessoal que visa facilitar/automatizar/agilizar o processo de pós instalação do NixOS modificando os arquivos configuration.nix e hardware-configuration.nix contidos em /etc/nixos" de Pororocka (Sandro Dias)
+#                   US = "This script is part of a personal project that aims to facilitate/automate/speed up the NixOS post-installation process by modifying the configuration.nix and hardware-configuration.nix files contained in /etc/nixos" by Pororocka (Sandro Dias)
+#                   ES = "Este script es parte de un proyecto personal que tiene como objetivo facilitar/automatizar/acelerar el proceso posterior a la instalación de NixOS modificando los archivos configuración.nix y hardware-configuration.nix contenidos en /etc/nixos" por Pororocka (Sandro Dias)
+#       OPTIONS:  ---
+#    COMPATIBLE:  NixOS 24.05
+#
+#  REQUIREMENTS:  [Dependências do sistema, bibliotecas necessárias, etc.]
+#          BUGS:  Relate problemas para [email@example.com]
+#         NOTES:  ---
+#        AUTHOR:  Dias, Sandro - pro.sandrodias@gmail.com
+#       COMPANY:  freedom
+#       VERSION:  1.0
+#       CREATED:  06/30/2024
+#      REVISION:  0 (ha! ha! ha!)
+#
+#==============================================================================
+
+# Variables and functions
